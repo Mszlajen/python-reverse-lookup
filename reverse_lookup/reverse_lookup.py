2b7588f0a3d96fccaef0b10c9c018a480385ed2d
Matias David Szlajen <mszlajen@dblandit.com>
2026-09-05 12:12:19 -0300

    Fix lookup for instances

--- reverse_lookup/reverse_lookup.py
+++ reverse_lookup/reverse_lookup.py
@@ -1,15 +1,22 @@
-from inspect import getmro, isfunction
+from inspect import getmro, isfunction, ismemberdescriptor
 from types import MethodType
 
 def lookup(self: object, mro: tuple[type, ...], name: str):
     for t in mro:
         try:
-            attr = getattr(t, name)
+            attr = object.__getattribute__(t, name)
         except AttributeError:
             continue
+        if ismemberdescriptor(attr):
+            return attr.__get__(self, t)
         if isfunction(attr):
             return MethodType(attr, self)
         return attr
+    
+    try:
+        return object.__getattribute__(self, '__dict__')[name]
+    except (AttributeError, KeyError) as e:
+        ...
     raise AttributeError(name=name, obj=self)
 
 class ReverseLookup():
@@ -22,17 +29,18 @@ class ReverseLookup():
         return list(reversed(cls.__mro__))
 
 class Inner:
-    __slots__ = ('obj', 'start_type')
+    __slots__ = ('__obj__', '__start_type__')
     
     def __init__(self, obj: object, start_type: type):
-        self.obj = obj
-        self.start_type = start_type
+        self.__obj__ = obj
+        self.__start_type__ = start_type
     
     def __getattribute__(self, name: str):
-        if name in ('start_type', 'obj'):
+        if name in ('__obj__', '__start_type__'):
             return super().__getattribute__(name)
-        start_type = self.start_type
-        obj = self.obj
+        start_type = self.__start_type__
+        obj = self.__obj__
         mro = object.__getattribute__(obj, '__class__').mro()
         new_mro = mro[mro.index(start_type)+1:]
         return lookup(obj, new_mro, name)
+        
